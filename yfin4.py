baf7f96aaca77e65660e695ff4479cd85131c220
unknown <tankpool895@gmail.com>
2025-10-28 22:08:36 +0530

    Initial commit

--- yfin4.py
+++ yfin4.py
@@ -1,6 +1,6 @@
 import yfinance as yf 
 
-aapl = yf.Ticker("AAPL")
-history = aapl.history(period="1mo", prepost=True)
-print(history)
-history.to_csv("AAPL_data.csv")
+aapl = yf.Ticker("AAPL") # This line creates a Ticker object for Apple Inc. using the yfinance library
+history = aapl.history(period="1mo", prepost=True) # This line retrieves the historical market data for Apple Inc. for the past month including pre and post market data
+print(history) #This line prints the historical market data for Apple Inc.
+history.to_csv("AAPL_data.csv") # This line saves the historical market data to a CSV file named "AAPL_data.csv"
